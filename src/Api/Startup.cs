a312768ddaf986e58232aaa085c4d30208b76231
longtq0807 <longtq0807@gmail.com>
2019-08-07 17:56:16 +0700

    Protecting an API using client credential

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -32,16 +32,6 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-
             app.UseAuthentication();
 
             app.UseMvc();
